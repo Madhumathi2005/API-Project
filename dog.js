343ab098c30fde2b3a34c6271c8d1610c3fb6400
Madhumathi2005 <153502123+Madhumathi2005@users.noreply.github.com>
2025-01-10 20:43:03 +0530

    Update dog.js

--- dog.js
+++ dog.js
@@ -1,12 +1,10 @@
 document.getElementById('getDog').addEventListener('click', function() {
-    // Fetch a random dog image from the Dog CEO API
     fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json()) // Convert response to JSON
+        .then(response => response.json()) 
         .then(data => {
-            // Display the image
             const dogImage = document.getElementById('dogImage');
-            dogImage.style.display = 'block'; // Show the image
-            dogImage.src = data.message; // Update the image src
+            dogImage.style.display = 'block'; 
+            dogImage.src = data.message; 
         })
         .catch(error => console.error('Error fetching dog image:', error));
 });
